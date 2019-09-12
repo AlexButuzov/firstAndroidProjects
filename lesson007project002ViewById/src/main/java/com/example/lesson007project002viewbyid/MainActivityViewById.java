187fd7ddbcfa9e66955367bccd0955d344f9d902
AlexButuzov <butuzov.ne@gmail.com>
2019-09-12 11:39:34 +0300

    lesseon008 ViewById empty

---
+++ lesson007project002ViewById/src/main/java/com/example/lesson007project002viewbyid/MainActivityViewById.java
@@ -0,0 +1,14 @@
+package com.example.lesson007project002viewbyid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityViewById extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view_by_id);
+    }
+}
