6efecf7c55cdeea9b2272f702995f04069252f17
AlexButuzov <butuzov.ne@gmail.com>
2019-09-17 09:52:55 +0300

    lesson008 ViewById done

--- lesson008project001ViewById/src/main/java/com/example/lesson008project001ViewById/MainActivityViewById.java
+++ lesson008project001ViewById/src/main/java/com/example/lesson008project001ViewById/MainActivityViewById.java
@@ -3,6 +3,8 @@ package com.example.lesson008project001ViewById;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivityViewById extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivityViewById extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view_by_id);
+        View myTextView = findViewById(R.id.myText);
+        TextView myTextInTextView = (TextView) myTextView;
+        myTextInTextView.setText("new Text in TextView");
+
+    }
+
+    public void changeText(View view) {
+        TextView myTextInTextView = (TextView) findViewById(R.id.myText);;
+        myTextInTextView.setText("new Text in TextView ->" + view.getHeight());
+
     }
 }
