fdd3c5e253e7151dde02c64e39b1ea42c2c26596
AlexButuzov <butuzov.ne@gmail.com>
2019-10-14 15:18:32 +0300

    lesson017 DynamicLayout2 is made via program.  done

---
+++ lesson017project001_DynamicLayout2/src/main/java/com/example/lesson017project001_dynamiclayout2/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.lesson017project001_dynamiclayout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    /*
+    llMain – вертикальный LinearLayout, в котором будут создаваться компоненты
+     */
+    private LinearLayout llMain;
+    /*
+    rgGravity-это RadioGroup, с тремя RadioButton (rbLeft, rbCenter, rbRight).
+    Этот компонент мы используем для выбора выравнивания создаваемого компонента
+     */
+    private RadioGroup rgGravity;
+    /*
+    etName – текстовое поле, здесь будем указывать текст,
+    который будет отображаться на созданном компоненте
+     */
+    private EditText etName;
+    /*
+    btnCreate – кнопка, запускающая процесс создания.
+     */
+    private Button btnCreate;
+    /*
+    btnClear – кнопка, стирающая все, что создали
+     */
+    private Button btnClear;
+
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        rgGravity = (RadioGroup) findViewById(R.id.rgGravity);
+        etName = (EditText) findViewById(R.id.etName);
+
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCreate:
+                // Создание LayoutParams c шириной и высотой по содержимому
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                        wrapContent, wrapContent);
+                // переменная для хранения значения выравнивания
+                // по умолчанию пусть будет LEFT
+                int btnGravity = Gravity.LEFT;
+                // определяем, какой RadioButton "чекнут" и
+                // соответственно заполняем btnGravity
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+                // переносим полученное значение выравнивания в LayoutParams
+                lParams.gravity = btnGravity;
+
+                // создаем Button, пишем текст и добавляем в LinearLayout
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew, lParams);
+
+                break;
+
+            case R.id.btnClear:
+                llMain.removeAllViews();
+                Toast.makeText(this, "Удалено", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+}
